3546782a2266718e6b7a096fb4f22836cf588ab7
minghan36 <mhua948@aucklanduni.ac.nz>
2023-03-21 22:29:00 +1300

    completed if statement in method printDatabase. Seperated into 0 profile, 1 profile and more than 1 profile.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -4,7 +4,7 @@ import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
 
-  private ArrayList profileList = new ArrayList();
+  private ArrayList<Profile> profileList = new ArrayList<Profile>();
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
@@ -13,16 +13,24 @@ public class InsuranceSystem {
   public void printDatabase() {
 
     int numberOfProfiles = profileList.size();
-    if (numberOfProfiles == 0){
-      
-
-    } 
     
+    if (numberOfProfiles == 0){
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");  
+    } else if(numberOfProfiles == 1){
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ".");
+      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage("1", profileList.get(0).getUsername(), Integer.toString(profileList.get(0).getAge()));
+    } else{
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(numberOfProfiles), "s", ".");  
+      for (int i = 0; i < numberOfProfiles; i++){
+        MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(Integer.toString(i), profileList.get(i).getUsername(), Integer.toString(profileList.get(i).getAge()));
+      }
     }
+    
+  }
 
     
     // TODO: Complete this method.
-  }
+  
 
   public void createNewProfile(String userName, String age) {
     // TODO: Complete this method.
