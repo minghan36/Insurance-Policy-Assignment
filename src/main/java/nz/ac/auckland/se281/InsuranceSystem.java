f1b94972d23b9f66e6313fe77d97853f3642f363
minghan36 <mhua948@aucklanduni.ac.nz>
2023-03-22 11:11:27 +1300

    Finished task one. Implemented try catch for Invalid ages. Check for unique username by iterating through arraylist.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -1,9 +1,6 @@
 package nz.ac.auckland.se281;
 
 import java.util.ArrayList;
-
-import org.eclipse.jgit.transport.CredentialItem.Username;
-
 import nz.ac.auckland.se281.Main.PolicyType;
 
 public class InsuranceSystem {
@@ -28,28 +25,42 @@ public class InsuranceSystem {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(numberOfProfiles), "s", ":");
       for (int i = 0; i < numberOfProfiles; i++) {
         MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
-            Integer.toString(i + 1),
-            profileList.get(i).getUserName(),
-            profileList.get(i).getAge());
+            Integer.toString(i + 1), profileList.get(i).getUserName(), profileList.get(i).getAge());
       }
     }
   }
 
   public void createNewProfile(String userName, String age) {
-    
+
+    // Changing userName to Title Case
     userName = userName.toLowerCase();
     char[] userNameChar = userName.toCharArray();
     userNameChar[0] = Character.toUpperCase(userNameChar[0]);
     userName = String.valueOf(userNameChar);
-    Profile user = new Profile(userName, age);
 
-    if (userName.length()<3){
-      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
-    } else if(profileList.contains(user)){
-      MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
-    } else if(!(Integer.parseInt(age)>=0)){
+    int numberOfProfiles = profileList.size();
+    // For loop to check that the username is unique, iterate through ArrayList
+    for (int i = 0; i < numberOfProfiles; i++) {
+      String currentProfileName = profileList.get(i).getUserName();
+      if (userName.equals(currentProfileName)) {
+        MessageCli.INVALID_USERNAME_NOT_UNIQUE.printMessage(userName);
+        return;
+      }
+    }
+    // Check for error return from parseInt. Meaning there was an Invalid age input.
+    try {
+      Integer.parseInt(age);
+    } catch (NumberFormatException e) {
       MessageCli.INVALID_AGE.printMessage(age, userName);
-    } else{
+      return;
+    }
+    // Check for name less than 3 letters and integer less than to 0 for age. If both false, new profile can be created and added to arrayList. 
+    if (userName.length() < 3) {
+      MessageCli.INVALID_USERNAME_TOO_SHORT.printMessage(userName);
+    } else if (!(Integer.parseInt(age) >= 0)) {
+      MessageCli.INVALID_AGE.printMessage(age, userName);
+    } else {
+      Profile user = new Profile(userName, age);
       profileList.add(user);
       MessageCli.PROFILE_CREATED.printMessage(userName, age);
     }
