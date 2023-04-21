4afd40cd3ee66253e44d60f62d9590ddb886d368
minghan36 <mhua948@aucklanduni.ac.nz>
2023-04-21 23:41:13 +1200

    Created new field to keep track of which profile is loaded. Completed loadProfile method. Adjusted printDatabase method, to print *** for the profile that is loaded. Edited createNewProfile method to prevent usage when a profile is loaded.

--- src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
+++ src/main/java/nz/ac/auckland/se281/InsuranceSystem.java
@@ -6,6 +6,7 @@ import nz.ac.auckland.se281.Main.PolicyType;
 public class InsuranceSystem {
 
   private ArrayList<Profile> profileList = new ArrayList<Profile>();
+  private int loadedProfileIndex = -1;
 
   public InsuranceSystem() {
     // Only this constructor can be used (if you need to initialise fields).
@@ -14,23 +15,55 @@ public class InsuranceSystem {
   public void printDatabase() {
 
     int numberOfProfiles = profileList.size();
-    // Using if else statements to print Database message depending on size of ArrayList (number of profiles).
+    // Using if else statements to print Database message depending on size of ArrayList (number of
+    // profiles).
+    // Print Database for when there is no profiles and only one profile.
     if (numberOfProfiles == 0) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("0", "s", ".");
     } else if (numberOfProfiles == 1) {
       MessageCli.PRINT_DB_POLICY_COUNT.printMessage("1", "", ":");
-      MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
-          "1", profileList.get(0).getUserName(), profileList.get(0).getAge());
-    } else {
-      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(numberOfProfiles), "s", ":");
-      for (int i = 0; i < numberOfProfiles; i++) {
+      if (loadedProfileIndex >= 0) { //Checks if the one profile is loaded
+        MessageCli.PRINT_DB_PROFILE_HEADER_SHORT.printMessage(
+            "*** ", "1", profileList.get(0).getUserName(), profileList.get(0).getAge());
+      } else {
         MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
-            Integer.toString(i + 1), profileList.get(i).getUserName(), profileList.get(i).getAge());
+            "1", profileList.get(0).getUserName(), profileList.get(0).getAge());
+      }
+    } else { //Printing for more than one profile in the database
+      MessageCli.PRINT_DB_POLICY_COUNT.printMessage(Integer.toString(numberOfProfiles), "s", ":");
+      if (loadedProfileIndex >= 0) { // For statement printing database when a profile is loaded
+        for (int i = 0; i < numberOfProfiles; i++) { 
+          if (i == loadedProfileIndex) {
+            MessageCli.PRINT_DB_PROFILE_HEADER_SHORT.printMessage(
+                "*** ",
+                Integer.toString(i + 1),
+                profileList.get(i).getUserName(),
+                profileList.get(i).getAge());
+          } else {
+            MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
+                Integer.toString(i + 1),
+                profileList.get(i).getUserName(),
+                profileList.get(i).getAge());
+          }
+        }
+      } else { // For statement printing database when a profile is not loaded
+        for (int i = 0; i < numberOfProfiles; i++) {
+          MessageCli.PRINT_DB_PROFILE_HEADER_MINIMAL.printMessage(
+              Integer.toString(i + 1),
+              profileList.get(i).getUserName(),
+              profileList.get(i).getAge());
+        }
       }
     }
   }
 
   public void createNewProfile(String userName, String age) {
+
+    if (loadedProfileIndex >= 0) {
+      MessageCli.CANNOT_CREATE_WHILE_LOADED.printMessage(
+          profileList.get(loadedProfileIndex).getUserName());
+      return;
+    }
 
     // Changing userName to Title Case
     userName = userName.toLowerCase();
@@ -68,7 +101,22 @@ public class InsuranceSystem {
   }
 
   public void loadProfile(String userName) {
-    // TODO: Complete this method.
+
+    // Changing userName to Title Case
+    userName = userName.toLowerCase();
+    char[] userNameChar = userName.toCharArray();
+    userNameChar[0] = Character.toUpperCase(userNameChar[0]);
+    userName = String.valueOf(userNameChar);
+
+    // For loop to scan for the username and load the profile if found.
+    for (int i = 0; i < profileList.size(); i++) {
+      if (profileList.get(i).getUserName().equalsIgnoreCase(userName)) {
+        loadedProfileIndex = i;
+        MessageCli.PROFILE_LOADED.printMessage(userName);
+        return;
+      }
+    }
+    MessageCli.NO_PROFILE_FOUND_TO_LOAD.printMessage(userName);
   }
 
   public void unloadProfile() {
